2a6876ba441e736318b87405ccfd300b4afc73fb
Micheal Chung <chungm7@hotmail.com>
2018-08-15 15:18:32 -0700

    Got bottom tabs functioning on MainPage

--- appFBLA2019/appFBLA2019/MainPage.xaml.cs
+++ appFBLA2019/appFBLA2019/MainPage.xaml.cs
@@ -7,7 +7,7 @@ using Xamarin.Forms;
 
 namespace appFBLA2019
 {
-    public partial class MainPage : ContentPage
+    public partial class MainPage : TabbedPage
     {
         public MainPage()
         {
