d098b21ee6e8a92229adc81d59d77996919f95fc
Micheal Chung <chungm7@hotmail.com>
2018-08-15 17:10:33 -0700

    Bottom tabs bugs issues

--- appFBLA2019/appFBLA2019/MainPage.xaml.cs
+++ appFBLA2019/appFBLA2019/MainPage.xaml.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
 
 namespace appFBLA2019
 {
-    public partial class MainPage : TabbedPage
-    {
-        public MainPage()
-        {
-            InitializeComponent();
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MainPage : Xamarin.Forms.TabbedPage
+	{
+		public MainPage ()
+		{
+			InitializeComponent ();
+            On<Xamarin.Forms.PlatformConfiguration.Android>().SetToolbarPlacement(ToolbarPlacement.Bottom);
         }
 
         private async void ButtonToLoginPage_Clicked(object sender, EventArgs e)
