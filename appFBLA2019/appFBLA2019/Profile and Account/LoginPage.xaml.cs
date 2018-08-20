8c5507e58b507a8f3dba514514f698f671fb1a15
Micheal Chung <chungm7@hotmail.com>
2018-08-20 09:46:10 -0700

    Fixed formatting issue

--- appFBLA2019/appFBLA2019/Profile and Account/LoginPage.xaml.cs
+++ appFBLA2019/appFBLA2019/Profile and Account/LoginPage.xaml.cs
@@ -17,80 +17,81 @@ namespace appFBLA2019
         private async void ButtonLogin_Clicked(object sender, EventArgs e)
         {
             Task login = Task.Run(async () =>
-           {
-               string username = this.EntryUsername.Text;
-               this.LabelMessage.Text = "Waiting...";
-               Task<bool> completedRequest = ServerConnector.QueryDB($"loginAccount/{username}/{this.EntryPassword.Text}/-");
-               //this was just to test if the UI would update while this happens (it doesn't) - but in theory it should
-               Device.BeginInvokeOnMainThread(() =>
-               {
-                   int i = 0;
-                   while (!completedRequest.IsCompleted)
-                   {
-                       //run a loading screen or something
-
-                       this.LabelMessage.Text = "Working... " + i;
-                       i++;
-                   }
-               });
-               
-               if (await completedRequest)
             {
-
-                string response = await ServerConnector.ReceiveFromDB();
-
-                if (response == "true/-")
+                string username = this.EntryUsername.Text;
+                this.LabelMessage.Text = "Waiting...";
+                Task<bool> completedRequest = ServerConnector.QueryDB($"loginAccount/{username}/{this.EntryPassword.Text}/-");
+                //this was just to test if the UI would update while this happens (it doesn't) - but in theory it should
+                Device.BeginInvokeOnMainThread(() =>
                 {
-                    this.LabelMessage.Text = "Login Successful!";
-                }
-                else if (response == "true/confirmEmail/-")
-                {
-                    this.LabelMessage.Text = "Please confirm your email.";
+                    int i = 0;
+                    while (!completedRequest.IsCompleted)
+                    {
+                        //run a loading screen or something
 
-                    var confirmationPage = new EmailConfirmationPage(username);
-                    confirmationPage.EmailConfirmed += this.OnEmailConfirmed;
-                       this.LabelMessage.Text = "Done!";
-                    await this.Navigation.PushModalAsync(confirmationPage);
-                }
-                else
+                        this.LabelMessage.Text = "Working... " + i;
+                        i++;
+                    }
+                });
+               
+                if (await completedRequest)
                 {
-                    this.LabelMessage.Text = "Login Failed: " + response.Split('/')[1];
+
+                    string response = await ServerConnector.ReceiveFromDB();
+
+                    if (response == "true/-")
+                    {
+                        this.LabelMessage.Text = "Login Successful!";
+                    }
+                    else if (response == "true/confirmEmail/-")
+                    {
+                        this.LabelMessage.Text = "Please confirm your email.";
+
+                        var confirmationPage = new EmailConfirmationPage(username);
+                        confirmationPage.EmailConfirmed += this.OnEmailConfirmed;
+                        this.LabelMessage.Text = "Done!";
+                        await this.Navigation.PushModalAsync(confirmationPage);
+                    }
+                    else
+                    {
+                        this.LabelMessage.Text = "Login Failed: " + response.Split('/')[1];
+                    }
+                    this.LabelMessage.Text = "Done!";
                 }
-                this.LabelMessage.Text = "Done!";
-            }
             else
             {
                 this.LabelMessage.Text = "Connection failed: Please try again.";
 
             }
-        });
+
+            });
         }
 
 
-    private async void ButtonToCreateAccountPage_Clicked(object sender, EventArgs e)
-    {
-        var createAccountPage = new CreateAccountPage();
-        createAccountPage.AccountCreated += this.OnAccountCreated;
-        await this.Navigation.PushAsync(createAccountPage);
-    }
-
-    public void OnEmailConfirmed(object source, EventArgs args)
-    {
-        this.LabelMessage.Text = "Login Successful!";
-    }
-
-    private void OnAccountCreated(object source, AccountCreatedEventArgs accountArgs)
-    {
-        this.EntryUsername.Text = accountArgs.Username;
-        if (accountArgs.EmailConfirmed)
+        private async void ButtonToCreateAccountPage_Clicked(object sender, EventArgs e)
         {
-            this.LabelMessage.Text = "Account created successfully!";
+            var createAccountPage = new CreateAccountPage();
+            createAccountPage.AccountCreated += this.OnAccountCreated;
+            await this.Navigation.PushAsync(createAccountPage);
         }
-        else
+
+        public void OnEmailConfirmed(object source, EventArgs args)
         {
-            this.LabelMessage.Text = "Account created successfully! " +
-                "Please confirm your email as soon as possible.";
+            this.LabelMessage.Text = "Login Successful!";
+        }
+
+        private void OnAccountCreated(object source, AccountCreatedEventArgs accountArgs)
+        {
+            this.EntryUsername.Text = accountArgs.Username;
+            if (accountArgs.EmailConfirmed)
+            {
+                this.LabelMessage.Text = "Account created successfully!";
+            }
+            else
+            {
+                this.LabelMessage.Text = "Account created successfully! " +
+                    "Please confirm your email as soon as possible.";
+            }
         }
     }
-}
 }
