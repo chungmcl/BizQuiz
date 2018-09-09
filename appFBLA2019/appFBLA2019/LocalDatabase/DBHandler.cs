4825db2151e870f8158c88eb7773dbcf27c8328e
Micheal Chung <chungm7@hotmail.com>
2018-09-09 15:52:38 -0700

    Added extension for file creation and etc

--- appFBLA2019/appFBLA2019/LocalDatabase/DBHandler.cs
+++ appFBLA2019/appFBLA2019/LocalDatabase/DBHandler.cs
@@ -8,11 +8,12 @@ namespace appFBLA2019
     static class DBHandler
     {
         public static GameDatabase Database { get; private set; }
+        private const string sqLiteExtension = ".db3";
         
         /// <summary>
         /// 
         /// </summary>
-        /// <param name="fileName">Name of the database file to be selected</param>
+        /// <param name="fileName">Name of the database file to be selected without extension</param>
         /// <returns>Bool representing successful database connection or not.</returns>
         public static bool SelectDatabase(string fileName)
         {
@@ -23,7 +24,7 @@ namespace appFBLA2019
                     Database = new GameDatabase(
                       Path.Combine(
                           Environment.GetFolderPath(
-                              Environment.SpecialFolder.LocalApplicationData), fileName)
+                              Environment.SpecialFolder.LocalApplicationData), fileName + sqLiteExtension)
                               , fileName);
                     return true;
                 }
