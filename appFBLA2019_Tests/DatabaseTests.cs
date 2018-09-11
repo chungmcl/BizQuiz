bf84f6d955005f50cdc6c41545f84a517b4f67e4
Micheal Chung <chungm7@hotmail.com>
2018-09-10 17:46:44 -0700

    Working on tests for database

---
+++ appFBLA2019_Tests/DatabaseTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using appFBLA2019;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace appFBLA2019_Tests
+{
+    [TestClass]
+    public class DatabaseTests
+    {
+        private const string sqLiteExtension = ".db3";
+        
+        [TestMethod]
+        public void TestFileCreation()
+        {
+            string fileName = "newFile";
+            DBHandler.SelectDatabase(fileName);
+
+            string path = Path.Combine(
+                          Environment.GetFolderPath(
+                              Environment.SpecialFolder.LocalApplicationData), fileName + sqLiteExtension);
+
+            Assert.IsTrue(Directory.GetFiles(path).Length != 0);
+        }
+    }
+}
