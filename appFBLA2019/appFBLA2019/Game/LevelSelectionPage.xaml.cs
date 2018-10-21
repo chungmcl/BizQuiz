bb0c53f873df49772fdd1cc817da071bdf4f66fa
Thomas Elley <thomas.m.elley@gmail.com>
2018-10-20 17:08:52 -0700

    LevelSelectionPage now has real functionality and connects smoothly to LevelCategoriesPage and TextGame

--- appFBLA2019/appFBLA2019/Game/LevelSelectionPage.xaml.cs
+++ appFBLA2019/appFBLA2019/Game/LevelSelectionPage.xaml.cs
@@ -9,19 +9,35 @@ using Xamarin.Forms.Xaml;
 
 namespace appFBLA2019
 {
+    
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class LevelSelectionPage : ContentPage
 	{
-		public LevelSelectionPage ()
+        public LevelSelectionPage(String[] levels)
 		{
             this.InitializeComponent ();
-		}
-
-        private async void History_Clicked(object sender, EventArgs e)
-        {
-            Level level = new Level("test");
-            await level.LoadQuestionsAsync();
-            await this.Navigation.PushAsync(new TextGame(level));
+            
+            for (int i = 0; i < levels.Count(); i++)
+            {
+                Button button = new Button
+                {
+                    Text = levels[i],
+                    VerticalOptions = LayoutOptions.CenterAndExpand,
+                    HorizontalOptions = LayoutOptions.CenterAndExpand
+                };
+                //adds an event handler to the button to deal with clicks
+                button.Clicked += async (object sender, EventArgs e) =>
+                    {
+                        Level level = new Level((sender as Button).Text);
+                        await level.LoadQuestionsAsync();
+                        await this.Navigation.PushAsync(new TextGame(level));
+                    };
+                
+                this.ButtonStack.Children.Add(button);
+            }
         }
+
+        public LevelSelectionPage()
+        { }
     }
 }
