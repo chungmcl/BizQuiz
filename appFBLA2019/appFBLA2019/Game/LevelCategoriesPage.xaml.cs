76d67a00cddb228ad0f1a078b2fb39bfe5c0928e
Thomas Elley <thomas.m.elley@gmail.com>
2019-01-21 15:40:36 -0800

    Made tab icons work and rounded butttons

--- appFBLA2019/appFBLA2019/Game/LevelCategoriesPage.xaml.cs
+++ appFBLA2019/appFBLA2019/Game/LevelCategoriesPage.xaml.cs
@@ -6,16 +6,22 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
 using Xamarin.Forms.Internals;
 
 namespace appFBLA2019
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class LevelCategoriesPage : TabbedPage
+	public partial class LevelCategoriesPage : Xamarin.Forms.TabbedPage
 	{
         public LevelCategoriesPage()
         {
             this.InitializeComponent();
-		}
+#if __ANDROID__
+            this.On<Xamarin.Forms.PlatformConfiguration.Android>().SetBarSelectedItemColor(Color.White);
+            this.On<Xamarin.Forms.PlatformConfiguration.Android>().SetBarItemColor(Color.DarkBlue);
+#endif
+            this.BarTextColor = Color.White;
+        }
 	}
 }
