3e97f57b0d0883862b7f7484ea282958515b3c8b
Thomas Elley <thomas.m.elley@gmail.com>
2019-02-04 10:12:24 -0800

    added LevelInfo.cs, a way to keep track of level info in a non-convuluted way. Every level gets one and it describes the level and its edit info

---
+++ appFBLA2019/appFBLA2019/LevelEditor/LevelInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace appFBLA2019.LevelEditor
+{
+    class LevelInfo : Realms.RealmObject
+    {
+        public string Author;
+        public string DateEdited;
+        public string Category;
+        public string LevelID
+    }
+}
