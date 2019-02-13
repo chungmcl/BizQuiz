bbba8f0ddf1db19ed37d1e9e5ba2f10235a70727
Micheal Chung <chungmcl@hotmail.com>
2019-02-13 15:44:53 -0800

    fix intellisense bug, workingon local levelinfo

---
+++ appFBLA2019/appFBLA2019/LocalDatabase/LevelInfo.cs
@@ -0,0 +1,32 @@
+﻿using Realms;
+using System;
+
+namespace appFBLA2019
+{
+    public class LevelInfo : RealmObject
+    {
+        public string DBId { get; set; }
+        public string AuthorName { get; set; }
+        public string LevelName { get; set; }
+        public string Category { get; set; }
+        public string LastModifiedDate { get; set; }
+        public bool NeedUploadSync { get; set; }
+        public bool NeedDownloadSync { get; set; }
+        public bool IsDeleted { get; set; }
+        
+        public LevelInfo() { }
+
+        public LevelInfo(string authorName, string levelName, string category)
+        {
+            this.DBId = Guid.NewGuid().ToString();
+            this.AuthorName = authorName;
+            this.LevelName = levelName;
+            this.Category = category;
+
+            this.LastModifiedDate = DateTime.Now.ToString();
+            this.NeedUploadSync = true;
+            this.NeedDownloadSync = false;
+            this.IsDeleted = false;
+        }
+    }
+}
---
+++ appFBLA2019/appFBLA2019/LocalDatabase/LevelRosterDatabase.cs
@@ -0,0 +1,46 @@
+﻿using Realms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace appFBLA2019
+{
+    public static class LevelRosterDatabase
+    {
+        private static Realm realmDB;
+        public static void Initialize()
+        {
+            try
+            {
+                RealmConfiguration rC = new RealmConfiguration(App.Path + "/" + "roster.realm");
+                realmDB = Realm.GetInstance(rC);
+            }
+            catch
+            {
+            }
+        }
+
+        public static void NewLevelInfo(LevelInfo levelInfo)
+        {
+            realmDB.Write(() =>
+            {
+                realmDB.Add(levelInfo);
+            });
+        }
+
+        public static void EditLevelInfo(LevelInfo levelInfo)
+        {
+            realmDB.Write(() =>
+            {
+                realmDB.Add(levelInfo, update: true);
+            });
+        }
+
+        public static LevelInfo GetLevelInfo(string DBId)
+        {
+            IQueryable<LevelInfo> levelInfos = realmDB.All<LevelInfo>();
+            return levelInfos.Where(levelInfo => levelInfo.DBId == DBId).First();
+        }
+    }
+}
