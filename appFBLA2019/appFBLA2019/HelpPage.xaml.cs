2c02178d9fe5addd0d4646b766e57f06a21188b1
Eric Hughes <erichughes00@outlook.com>
2019-02-19 17:51:29 -0800

    not ignored

---
+++ appFBLA2019/appFBLA2019/HelpPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿//BizQuiz App 2019
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace appFBLA2019
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HelpPage : CarouselPage
+    {
+        public HelpPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
