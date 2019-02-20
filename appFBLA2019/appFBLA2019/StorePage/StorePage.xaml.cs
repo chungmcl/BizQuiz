f5e93dd13429ebe88b8b8de72d02c6062cf9ac90
Eric Hughes <erichughes00@outlook.com>
2019-02-19 16:07:27 -0800

    changes

--- appFBLA2019/appFBLA2019/StorePage/StorePage.xaml.cs
+++ appFBLA2019/appFBLA2019/StorePage/StorePage.xaml.cs
@@ -12,12 +12,14 @@ namespace appFBLA2019
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class StorePage : ContentPage
 	{
+        private int chunkNum;
+        private bool end;
 
         /// <summary>
         /// Adds a level to the search stack given a LevelInfo
         /// </summary>
         /// <param name="level"></param>
-        private void AddLevel(LevelInfo level)
+        private void AddLevel(SearchInfo level)
         {
             Frame levelFrame = new Frame
             {
@@ -65,7 +67,7 @@ namespace appFBLA2019
 
             Label levelAuthor = new Label
             {
-                Text = "Created by: " + level.AuthorName,
+                Text = "Created by: " + level.Author,
             };
             frameStack.Children.Add(levelAuthor);
 
@@ -107,13 +109,13 @@ namespace appFBLA2019
         }
 
         // Temporary
-        private List<LevelInfo> Search(string text)
-        {
-            List <LevelInfo> testInfo = new List<LevelInfo>();
-            testInfo.Add(new LevelInfo { DBId = "TestDBID", AuthorName = "TestAuthor", LevelName = "TestLevel", Category = "FBLA General" });
-            testInfo.Add(new LevelInfo { DBId = "TestDBID2", AuthorName = "TestAuthor2", LevelName = "TestLevel2", Category = "FBLA General" });
-            return testInfo;
-        }
+        //private List<LevelInfo> Search(string text)
+        //{
+        //    List <LevelInfo> testInfo = new List<LevelInfo>();
+        //    testInfo.Add(new LevelInfo { DBId = "TestDBID", AuthorName = "TestAuthor", LevelName = "TestLevel", Category = "FBLA General" });
+        //    testInfo.Add(new LevelInfo { DBId = "TestDBID2", AuthorName = "TestAuthor2", LevelName = "TestLevel2", Category = "FBLA General" });
+        //    return testInfo;
+        //}
 
         public StorePage ()
 		{
@@ -129,11 +131,30 @@ namespace appFBLA2019
         {
             // Delete what was in there previously
             this.SearchedStack.Children.Clear();
-            foreach (LevelInfo level in Search(this.SearchBar.Text))
-            {
-                this.AddLevel(level);
-            }
+            this.end = false;
+            this.Search(1);
         }
+
+        private void Search(int chunkNum)
+        {
+            int i = 0;
+            foreach (string[] level in ServerOperations.GetLevelsByLevelName(this.SearchBar.Text, chunkNum))
+            {
+                this.AddLevel(new SearchInfo
+                {
+                    DBId = level[0],
+                    Author = level[1],
+                    LevelName = level[2],
+                    Category = level[3],
+                    SubCount = int.Parse(level[4])
+                });
+                i++;
+            }
+            if (i < 20)
+                this.end = true;
+        }
+
+        
 
         private void SearchBar_TextChanged(object sender, TextChangedEventArgs e)
         {
@@ -143,6 +164,28 @@ namespace appFBLA2019
         protected override void OnDisappearing()
         {
             this.SearchedStack.Children.Clear();
+        }
+        
+
+        private class SearchInfo
+        {
+            public string DBId { get; set; }
+            public string Author { get; set; }
+            public string LevelName { get; set; }
+            public string Category { get; set; }
+            public int SubCount { get; set; }
+
+        }
+
+        private void ScrollSearch_Scrolled(object sender, ScrolledEventArgs e)
+        {
+            ScrollView scrollView = sender as ScrollView;
+            double scrollingSpace = scrollView.ContentSize.Height - scrollView.Height;
+
+            if (scrollingSpace <= e.ScrollY && !this.end)
+            {
+                this.Search(this.chunkNum++);
+            }
         }
     }
 }
