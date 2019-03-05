b620613439c2313994cd6a2b77fd000752ee1034
Micheal Chung <chungmcl@hotmail.com>
2019-03-04 17:13:36 -0800

    Added missing gitignore and changed code for assets

---
+++ appFBLA2019/appFBLA2019/ICloseApplication.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace appFBLA2019
+{
+    public interface ICloseApplication
+    {
+        void CloseApplication();
+    }
+}
