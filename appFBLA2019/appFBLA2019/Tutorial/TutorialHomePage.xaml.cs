a187c276d809117d4f01c563407569af15de652b
Eric Hughes <erichughes00@outlook.com>
2019-05-02 16:40:23 -0700

    staged tutorial

---
+++ appFBLA2019/appFBLA2019/Tutorial/TutorialHomePage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace appFBLA2019
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TutorialHomePage : ContentPage
+    {
+        public TutorialHomePage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Next_Clicked(object sender, EventArgs e)
+        {
+            await this.Navigation.PushAsync(new TutorialGame());
+        }
+    }
+}
